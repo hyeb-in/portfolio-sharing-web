183f59ce715d86bfa712eafd293c1ab9671285b8
leehyebin <970905hyebin@gmail.com>
2023-07-12 02:48:02 +0900

    education 컴포넌트

--- front/src/components/user/UserEducationEdit.js
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Container, Row } from "react-bootstrap";
-
-
-export default UserEducationEdit
