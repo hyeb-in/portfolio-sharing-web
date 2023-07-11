4e9e79a017d11c902e1a89b078187c8963a7c5de
[AI8/1반] 이창근 <echk1212@gmail.com>
2023-07-11 08:19:04 +0000

    추가 파일에 export default추가했습니다.

--- front/src/components/main/Galimgil.js
+++ front/src/components/main/Galimgil.js
@@ -1,3 +1,10 @@
+//메인화면(갈림길 컨포넌트입니다.)
 import React, { useEffect, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Row } from "react-bootstrap";
+
+const search = ()=>{
+    return
+}
+
+export default Galimgil
