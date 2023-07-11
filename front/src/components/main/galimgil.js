9211551f55c6dcb8a388c501576bdf215499ff75
[AI8/1반] 이창근 <echk1212@gmail.com>
2023-07-11 08:11:44 +0000

    추가 컴포넌트 파일입니다.

---
+++ front/src/components/main/galimgil.js
@@ -0,0 +1,4 @@
+//메인화면(갈림길 컨포넌트입니다.)
+import React, { useEffect, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Row } from "react-bootstrap";
---
+++ front/src/components/user/UserEductionEdit.js
@@ -0,0 +1,3 @@
+import React, { useEffect, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Row } from "react-bootstrap";
