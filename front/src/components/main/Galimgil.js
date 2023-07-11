c887b379820df8748142f6539a8117b2b7e177e5
[AI8/1반] 이창근 <echk1212@gmail.com>
2023-07-11 08:43:49 +0000

    갈림길 컴포넌트 1차

--- front/src/components/main/Galimgil.js
+++ front/src/components/main/Galimgil.js
@@ -3,8 +3,18 @@ import React, { useEffect, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Row } from "react-bootstrap";
 
-const search = ()=>{
-    return
+//클릭 시 직종별 포트폴리오를 볼 수 있는 컴포넌트입니다.
+const byOccupation = async (e)=> {
+    e.preventDefault();
+
+    try {
+        await Api.post("네트워크 엔드포인트 넣을 예정", {
+            
+        })
+
+    } catch (err) {
+ console.log(err);
+    }
 }
 
 export default Galimgil
