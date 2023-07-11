2ee8d3114c0a2cb4632e18c9c84ae6fa936673d9
devchaeyoung <chaemaa00@gmail.com>
2023-07-12 02:58:21 +0900

    프론트 컴포넌트 파일 목록 정리

--- front/src/components/main/Categories.js
+++ front/src/components/main/Categories.js
@@ -4,14 +4,11 @@ import { useNavigate } from "react-router-dom";
 import { Container, Row } from "react-bootstrap";
 
 //클릭 시 직종별 포트폴리오를 볼 수 있는 컴포넌트입니다.
-const Categories = async (e) => {
-  e.preventDefault();
-
-  try {
-    await Api.post("네트워크 엔드포인트 넣을 예정", {});
-  } catch (err) {
-    console.log(err);
-  }
+const Categories = () => {
+  return (
+    <div>
+      <p>갈림길에서 떠다니는 박스 클릭했을 때 이동하는 컨테이너입니다.</p>
+    </div>
+  );
 };
-
 export default Categories;
