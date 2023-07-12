76ec58f94bb38732a5757e1e3649244880e74507
devchaeyoung <chaemaa00@gmail.com>
2023-07-13 00:36:06 +0900

    Docs:기술스택 수정

--- front/src/components/user-Skill-Stack/UserSkillStack.js
+++ front/src/components/user-Skill-Stack/UserSkillStack.js
@@ -5,31 +5,7 @@ import { Container, Col, Row, Form, Button } from "react-bootstrap";
 //input search 넣기
 const UserSkillStack = () => {
   return (
-    <div>
-      <div className="skill-tag-list"></div>
-      <input type="search"></input>
-      <p className="skill-tag">Java</p>
-      <p className="skill-tag">JavaScript</p>
-      <p className="skill-tag">Spring</p>
-      <p className="skill-tag">HTML/CSS</p>
-      <p className="skill-tag">jQuery</p>
-      <p className="skill-tag">JSP</p>
-      <p className="skill-tag">Vue.js</p>
-      <p className="skill-tag">Oracle</p>
-      <p className="skill-tag">MySQL</p>
-      <p className="skill-tag">React</p>
-      <p className="skill-tag">Spring Boot</p>
-      <p className="skill-tag">PHP</p>
-      <p className="skill-tag">Python</p>
-      <p className="skill-tag">Node.js</p>
-      <p className="skill-tag">C#</p>
-      <p className="skill-tag">swift</p>
-      <p className="skill-tag">Kotlin</p>
-      <p className="skill-tag">MSSQL</p>
-      <p className="skill-tag">React Native</p>
-      <p className="skill-tag">전자정부 프레임워크</p>
-    </div>
-  );
+);
 };
 
 export default UserSkillStack;
--- front/src/components/user-Skill-Stack/UserSkillStackEdit.js
+++ front/src/components/user-Skill-Stack/UserSkillStackEdit.js
@@ -5,8 +5,41 @@ import { Container, Row } from "react-bootstrap";
 const UserStackEdit = (e) => {
   e.preventDefault();
 
-  const stackTag = () => {};
-  return <></>;
+  const skillList = [
+    "Java",
+    "JavaScript",
+    "Spring",
+    "HTML/CSS",
+    "jQuery",
+    "JSP",
+    "Vue.js",
+    "Oracle",
+    "MySQL",
+    "React",
+    "Spring Boot",
+    "PHP",
+    "Python",
+    "Node.js",
+    "C#",
+    "Swift",
+    "Kotlin",
+    "MySQL",
+    "React Native",
+    "전자정부 프레임워크",
+  ];
+
+  return (
+    <div>
+      <input type="text"></input>
+      <div className="skill-tag-list">
+        {skillList.map((skill) => (
+          <p className="skilll-tag" key={skill}>
+            {skill}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default UserStackEdit;
