9a381cd7c581696c2b139fa45b7a3db0125885a7
leehyebin <970905hyebin@gmail.com>
2023-07-12 22:00:54 +0900

    스크롤 버튼 추가 및 변경

--- front/src/components/tool/ScrollToTop.js
+++ front/src/components/tool/ScrollToTop.js
@@ -1,12 +1,11 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
+  const goTop = () => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+  };
 
-  return null;
+  return <Button onClick={goTop}>스크롤</Button>;
 }
