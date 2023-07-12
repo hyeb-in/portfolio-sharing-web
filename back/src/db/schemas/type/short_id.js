cdf587e6b633d23b0f64d4f3522a39ccd7986510
[AI8/1반] 조대찬 <daechan476@gmail.com>
2023-07-12 17:39:29 +0900

    Fix:short_id 버젼 다운그레이드

--- back/src/db/schemas/type/short_id.js
+++ back/src/db/schemas/type/short_id.js
@@ -3,7 +3,7 @@
 // 최신버전은 require 를 지원하지 않음. import만 지원함
 // 그렇기 때문에 npm i nanoid@2.1.11 를 사용
 // 현재 나노아이디를 리콰이어할 때 함수형으로 받으면 오류가생김. 왜인지는 아직 모르겠음
-import { nanoid } from "nanoid";
+const nanoid = require("nanoid");
 
 const shortId = {
     type: String,
