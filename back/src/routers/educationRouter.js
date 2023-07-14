2c83c8efd923b060d0baeeb2d4a9c776cabe2eb6
{이창근} <{echk1212@gmail.com}>
2023-07-14 14:18:48 +0900

    q

--- back/src/routers/educationRouter.js
+++ back/src/routers/educationRouter.js
@@ -1,77 +1,23 @@
 import { Router } from "express";
 import { login_required } from "../middlewares/login_required";
-import { educationAuthService } from "../services/educationService";
+import { 
+    deleteEducation,
+    getEducation, 
+    postEducation, 
+    putEducation, 
+    userGetEducation 
+} from "../controllers/education-controller";
 const educationAuthRouter = Router();
 
-educationAuthRouter.post("/education",
-    login_required,
-    async (req, res, next) => {
-        try {
-            const author = req.currentUserId;
-            const { schoolName, major, crnt } = req.body;
-
-        const newEducation = await educationAuthService.addEducation(
-                schoolName,
-                major,
-                crnt,
-                author
-            );
-            res.status(201).json(newEducation);
-        } catch (error) {
-            next(error);
-        }
-    }
-);
+educationAuthRouter.post("/education", login_required, postEducation);
 
 
-educationAuthRouter.get("/education",
-    login_required,
-    async (req, res, next) => {
-        try {
+educationAuthRouter.get("/education", login_required, userGetEducation);
 
-            const education = await educationAuthService.getEducation(req.currentUserId);
+educationAuthRouter.get("/education/:id", login_required, getEducation);
 
-            res.status(200).send(education);
-        } catch (error) {
-            next(error);
-        }
-    }
-);
+educationAuthRouter.put("/education/:id", login_required, putEducation);
 
-educationAuthRouter.put("/education/:id",
-    login_required,
-    async (req, res, next) => {
-        try {
-            const educationId = req.params.id;
-            const schoolName = req.body.schoolName ?? null;
-            const major = req.body.major ?? null;
-            const crnt = req.body.crnt ?? null;
-
-
-        const toUpdate ={schoolName, major, crnt};
-        
-        const updatedEducation = await educationAuthService.setEducation({educationId,toUpdate});
-
-        
-
-            res.status(201).send(updatedEducation);
-        } catch (error) {
-            next(error);
-        }
-    }
-);
-
-
-educationAuthRouter.delete('/education/:id',
-    login_required,
-    async (req,res,next)=>{
-        try{
-
-            const deleteEducation = await educationAuthService.deleteEducation(req.params.id);
-            res.status(200).send(deleteEducation);
-        }catch(error){
-            next(error);
-        }
-    });
+educationAuthRouter.delete('/education/:id', login_required, deleteEducation);
 
 export { educationAuthRouter };
