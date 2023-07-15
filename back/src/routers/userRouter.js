49150506072c9334a40ba03969024263f5dabfc9
devchaeyoung <chaemaa00@gmail.com>
2023-07-16 05:32:28 +0900

    Fix: 오타 수정

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -4,16 +4,16 @@ import { createValidator } from "express-joi-validation";
 import { userBodySchema } from "../utils/validatorSchema/userBodySchema";
 
 import {
-	singUpUser,
-	loginUser,
-	getUsers,
-	currentUser,
-	updateUser,
-	getUser,
-	userJWT,
-	logoutUser,
-	deleteUser,
-	setPassword,
+  singUpUser,
+  loginUser,
+  getUsers,
+  currentUser,
+  updateUser,
+  getUser,
+  userJWT,
+  logoutUser,
+  deleteUser,
+  setPassword,
 } from "../controllers/user-controller";
 
 const validator = createValidator();
@@ -22,7 +22,7 @@ const userAuthRouter = Router();
 
 // 회원가입 라우터
 userAuthRouter.post("/user/register", singUpUser);
-보;
+
 // 로그인 라우터
 userAuthRouter.post("/user/login", loginUser);
 
@@ -33,10 +33,10 @@ userAuthRouter.get("/userlist", login_required, getUsers);
 userAuthRouter.get("/user/current", login_required, currentUser);
 
 userAuthRouter
-	.route("/user/:id")
-	.get(login_required, getUser) // 유저 조회
-	.put(login_required, updateUser) // 유저 정보 수정
-	.delete(login_required, deleteUser); // 회원 탈퇴
+  .route("/user/:id")
+  .get(login_required, getUser) // 유저 조회
+  .put(login_required, updateUser) // 유저 정보 수정
+  .delete(login_required, deleteUser); // 회원 탈퇴
 
 // jwt 토큰 기능 확인용, 삭제해도 되는 라우터임.
 userAuthRouter.get("/afterlogin", login_required, userJWT);
