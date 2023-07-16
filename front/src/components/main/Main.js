2669ccf0d7d280009c699a9369a6758b27052c41
devchaeyoung <chaemaa00@gmail.com>
2023-07-17 03:23:05 +0900

    Feat:스크롤 Main컴포넌트 만들기

--- front/src/components/main/Main.js
+++ front/src/components/main/Main.js
@@ -1,15 +1,69 @@
-//메인화면(갈림길 컨포넌트입니다.)
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
-
+import "./main.css";
 /**
  *웹 페이지를 소개하는 메인 컴포넌트 입니다.
  */
 function Main() {
+  // let observer = new IntersectionObserver((e) => {
+  //   e.forEach((box) => {
+  //     if (box.isIntersecting) {
+  //       box.target.opacity = 1;
+  //     } else {
+  //       box.target.style.opacity = 1;
+  //     }
+  //   });
+  // });
+  // let findDiv = document.querySelectorAll("div");
+  // observer.observe(findDiv[0]);
+
+  const [position, setPosition] = useState(0);
+  function onScroll() {
+    setPosition(window.scrollY);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
   return (
-    <>
-      <Container>웹 페이지를 소개하는 메인 컴포넌트입니다.</Container>
-    </>
+    <Container className="body">
+      <div
+        className="bg-bg1"
+        style={{ backgroundPositionY: position / 2 }}
+      ></div>
+      <div className="bg bg2" style={{ backgroundPositionY: position / -3 }}>
+        {/* 이거 왜안되는거지? */}
+        <img src={process.env.PUBLIC_URL + "/img/forest2.jpg"} width="300px" />
+      </div>
+      <div
+        className="tree"
+        style={{
+          transform: `translateX(${position}px)`,
+        }}
+      >
+        <p>취업의 숲에 오신것을 환영합니다</p>
+      </div>
+      <div
+        className="tree2"
+        style={{
+          transform: `translateX(${-position}px)`,
+        }}
+      >
+        <p>함께 이뤄요</p>
+      </div>
+      <div
+        className="intro"
+        style={{
+          opacity: (position - 830) / 50,
+        }}
+      >
+        <h1>취업의 숲</h1>
+      </div>
+    </Container>
   );
 }
+
 export default Main;
--- front/src/components/main/WelcomeForest.js
+++ front/src/components/main/WelcomeForest.js
@@ -1,23 +1,17 @@
-import React, { useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import { Container, Col, Row, Form, Button } from "react-bootstrap";
-
-import * as Api from "../../api";
-import { DispatchContext } from "../../App";
 
 //클릭 시 직종별 포트폴리오를 볼 수 있는 컴포넌트입니다.
 const WelcomeForest = () => {
-
-
   return (
     <Container>
       <Row className="justify-content-md-center mt-5">
         <Col lg={8}>
-        <Form>
-          <Form.Label>모여봐요</Form.Label><br />
-          <Form.Label>취업의 숲</Form.Label>
+          <Form>
+            <Form.Label>모여봐요</Form.Label>
+            <br />
+            <Form.Label>취업의 숲</Form.Label>
 
-          
             <Form.Group as={Row} className="mt-3 text-center">
               <Col sm={{ span: 20 }}>
                 <Button variant="primary" type="submit">
