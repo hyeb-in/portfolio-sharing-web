83d1c4dc210f79a085e3429a3c8c852168b832a2
devchaeyoung <chaemaa00@gmail.com>
2023-07-17 20:22:07 +0900

    Docs: networkNavigationBar 효율업!

--- front/src/components/nav/NetworkNavigationBar.js
+++ front/src/components/nav/NetworkNavigationBar.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "./NetworkNavigationBar.style.css";
 
-function Nav({ skills, onClick }) {
+function Nav({ devMajor, onClick }) {
   return (
     <nav className="network-navigation">
-      {skills.map((skill) => (
+      {devMajor.map((skill) => (
         <span
           className="network-nav-item"
           key={skill.id}
@@ -16,24 +16,26 @@ function Nav({ skills, onClick }) {
     </nav>
   );
 }
+const devMajor = [
+  { id: 1, title: "프론트", link: "#front" },
+  { id: 2, title: "백", link: "#back" },
+  { id: 3, title: "데브옵스", link: "#devops" },
+  { id: 4, title: "데이터분석", link: "#data-analysis" },
+  { id: 5, title: "Ai", link: "#ai" },
+  { id: 6, title: "웹", link: "#web" },
+  { id: 7, title: "앱", link: "#app" },
+];
+
 const NetworkNavigataionBar = (props) => {
   const [link, setLink] = useState("");
 
-  const skills = [
-    { id: 1, title: "프론트", link: "#front" },
-    { id: 2, title: "백", link: "#back" },
-    { id: 3, title: "데브옵스", link: "#devops" },
-    { id: 4, title: "데이터분석", link: "#data-analysis" },
-    { id: 5, title: "Ai", link: "#ai" },
-    { id: 6, title: "웹", link: "#web" },
-    { id: 7, title: "앱", link: "#app" },
-  ];
+  //Link가 변경될때마다 밑에가 계속 그려진다.
 
   const onClickLink = (link) => {
     setLink(link);
   };
 
-  return <Nav skills={skills} onClick={onClickLink} />;
+  return <Nav devMajor={devMajor} onClick={onClickLink} />;
 };
 
 export default NetworkNavigataionBar;
