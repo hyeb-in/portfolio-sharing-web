98e9faa30e99b4ff45f0d8ebafbc9da8a6802330
devchaeyoung <chaemaa00@gmail.com>
2023-07-21 23:13:45 +0900

    Docs:console.log

--- front/src/components/Portfolio.js
+++ front/src/components/Portfolio.js
@@ -59,7 +59,6 @@ function Portfolio() {
   useEffect(() => {
     // 전역 상태의 user가 null이라면 로그인이 안 된 상태이므로, 로그인 페이지로 돌림.
     if (!userState.user && !params.id && !sessionStorage.getItem("userToken")) {
-      console.log("이거 되나 ", userState.user);
       navigate("/login", { replace: true });
       return;
     }
